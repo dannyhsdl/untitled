d0a670f0127805c31aef18e7eae0d5119aea5889
dannyhsdl <danwong2018@outlook.com>
2019-01-11 12:17:56 +0800

    用谷歌浏览器打开某个网站并执行相应的 操作

---
+++ 网页自动化03.py
@@ -0,0 +1,38 @@
+#from selenium import webdriver
+# from selenium.webdriver.support.select import Select
+#
+# driver=webdriver.Chrome('F:\chrome.exe')
+# driver.get('https://www.timeanddate.com')
+#
+# selectelements=driver.find_element_by_id('month')
+# months=Select(selectelements)
+# months.select_by_visible_text('December')
+#
+# countryelements=driver.find_element_by_id('country')
+# countries=Select(countryelements)
+# countries.select_by_visile_text('Taiwan')
+#
+# driver.find_element_by_xpath("//input[@value='View Calendar']").click()
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+import time
+
+driver=webdriver.Chrome("F:\chromedriver.exe")
+driver.fullscreen_window()
+driver.get("https://www.timeanddate.com")
+
+selectelements=driver.find_element_by_id('month')
+months=Select(selectelements)
+months.select_by_visible_text('February')
+
+countrieselements=driver.find_element_by_id('country')
+countries=Select(countrieselements)
+countries.select_by_visible_text('China')
+
+driver.find_element_by_xpath("//form[@id='cf']//input[@value='View Calendar']").click()
+
+time.sleep(10)
+driver.quit()
+
+
+
